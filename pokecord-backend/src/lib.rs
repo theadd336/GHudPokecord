a6ad959840223e90c419ee1b6b7d2b4148a2e08c
Paul Addonizio <pauladdonizio@gmail.com>
2021-05-09 16:04:18 -0400

    Docker env setup

--- pokecord-backend/src/lib.rs
+++ pokecord-backend/src/lib.rs
@@ -1,11 +1,19 @@
-pub fn my_test() {
-    println!("Hello world");
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+/// add(a, b, /)
+/// --
+///
+/// This function adds two unsigned 64-bit integers.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pokecord_backend(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    Ok(())
 }
--- scripts/start_pokecord_discord.sh
+++ scripts/start_pokecord_discord.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp /app/pokecord-backend/target/debug/libpokecord_backend.so /app/pokecord-discord/pokecord_backend.so
-python3.7 -m pokecord-discord
+cp /app/pokecord-backend/target/debug/libpokecord_backend.so /app/pokecord/pokecord_backend.so
+python3.9 -m pokecord
